70dd077ac86b315518be16e228deb33d928cc814
Zaza <47594798+Zazuan@users.noreply.github.com>
2019-02-28 18:30:18 +0300

    Add files via upload

---
+++ lab1.2.py
@@ -0,0 +1,13 @@
+import random
+print ("k:")
+k = int(input())
+a = random.randint(0,k)
+print ("b:")
+b = int(input())
+while a <> b:
+    print("incorrect word")
+    b = int(input())
+print ("a = %d" % (b))
+
+
+
